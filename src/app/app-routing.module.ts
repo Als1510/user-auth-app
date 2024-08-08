beab9f07ec3fd57ef0e3b7f95efd16afd7169387
Anshul Sharma <anshul.sharma@alectio.com>
2024-08-08 22:22:56 +0530

    feat: Use hash strategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadingStrategy, RouterModule, Routes } from '@angular/router';
 import { CheckUserComponent } from './components/check-user/check-user.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
@@ -9,10 +9,11 @@ const routes: Routes = [
   { path: 'user-check', component: CheckUserComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
+  { path: '**', redirectTo: '/user-check' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadingStrategy, useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
